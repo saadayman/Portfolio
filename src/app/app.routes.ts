fe259b914414e428915f5d02f2626dd0fa30c6b8
sayman <sayman@nixpend.com>
2024-06-19 01:57:45 +0300

    add testing endpoint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,10 @@ export const routes: Routes = [
         loadComponent:()=>import('./home-page/home/home.component').then(c=>c.HomeComponent)
     },
     {
+        path:'test',
+        loadComponent:()=>import('./home-page/home/home.component').then(c=>c.HomeComponent)
+    },
+    {
         path:'',
         pathMatch:'full',
         redirectTo:'home'
