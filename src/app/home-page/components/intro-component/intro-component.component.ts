2cab98fadbd0f94141952426afcb4140c7e66db2
sayman <sayman@nixpend.com>
2024-06-22 00:37:56 +0300

    css

--- src/app/home-page/components/intro-component/intro-component.component.ts
+++ src/app/home-page/components/intro-component/intro-component.component.ts
@@ -1,11 +1,12 @@
 import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-intro-component',
   standalone: true,
-  imports: [NgOptimizedImage,TranslateModule],
+  imports: [NgOptimizedImage,TranslateModule,NgbTooltipModule],
   templateUrl: './intro-component.component.html',
   styleUrl: './intro-component.component.scss',
   changeDetection:ChangeDetectionStrategy.OnPush
