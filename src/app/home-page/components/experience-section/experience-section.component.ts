18ae3c11dadd0aad0ae99efd395c6b67bc829357
sayman <sa467563@gmail.com>
2025-07-26 14:59:37 +0300

    change skills component view

--- src/app/home-page/components/experience-section/experience-section.component.ts
+++ src/app/home-page/components/experience-section/experience-section.component.ts
@@ -1,15 +1,80 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+
+interface Experience {
+  id: string;
+  dates: string;
+  title: string;
+  company: string;
+  location: string;
+  icon: string;
+  companyIcon: string;
+  points: string[];
+  isCurrent?: boolean;
+  isActive?: boolean;
+}
 
 @Component({
   selector: 'app-experience-section',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './experience-section.component.html',
   styleUrl: './experience-section.component.scss',
   changeDetection:ChangeDetectionStrategy.OnPush
 
 })
 export class ExperienceSectionComponent {
-
+  experiences: Experience[] = [
+    {
+      id: 'several-brand',
+      dates: 'EXPERIENCE.SEVERAL_BRAND.DATES',
+      title: 'EXPERIENCE.SEVERAL_BRAND.TITLE',
+      company: 'EXPERIENCE.SEVERAL_BRAND.COMPANY',
+      location: 'Delaware',
+      icon: 'fa-solid fa-laptop fa-bounce',
+      companyIcon: 'fa-solid fa-building',
+      points: [
+        'EXPERIENCE.SEVERAL_BRAND.POINT1',
+        'EXPERIENCE.SEVERAL_BRAND.POINT2',
+        'EXPERIENCE.SEVERAL_BRAND.POINT3'
+      ],
+      isCurrent: true,
+      isActive: true,
+    },
+    {
+      id: 'nixpend',
+      dates: 'EXPERIENCE.NIXPEND.DATES',
+      title: 'EXPERIENCE.NIXPEND.TITLE',
+      company: 'EXPERIENCE.NIXPEND.COMPANY',
+      location: 'Jordan',
+      icon: 'fa-solid fa-laptop fa-bounce',
+      companyIcon: 'fa-solid fa-building',
+      points: [
+        'EXPERIENCE.NIXPEND.POINT1',
+        'EXPERIENCE.NIXPEND.POINT2',
+        'EXPERIENCE.NIXPEND.POINT3'
+      ],
+      isActive: true,
+    },
+    {
+      id: 'eureka',
+      dates: 'EXPERIENCE.EUREKA.DATES',
+      title: 'EXPERIENCE.EUREKA.TITLE',
+      company: 'EXPERIENCE.EUREKA.COMPANY',
+      location: 'Jordan',
+      icon: 'fa-solid fa-person-chalkboard fa-bounce',
+      companyIcon: 'fa-solid fa-school',
+      points: [
+        'EXPERIENCE.EUREKA.POINT1',
+        'EXPERIENCE.EUREKA.POINT2',
+        'EXPERIENCE.EUREKA.POINT3',
+        'EXPERIENCE.EUREKA.POINT4',
+        'EXPERIENCE.EUREKA.POINT5',
+        'EXPERIENCE.EUREKA.POINT6',
+        'EXPERIENCE.EUREKA.POINT7'
+      ],
+      isActive: true,
+    }
+  ].filter(experience => experience.isActive) ;
 }
--- src/app/home-page/components/skills/skills.component.ts
+++ src/app/home-page/components/skills/skills.component.ts
@@ -1,16 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import {CdkDrag, CdkDragDrop, CdkDragPreview, CdkDropList, moveItemInArray} from '@angular/cdk/drag-drop';
-import { CommonModule } from '@angular/common';
+
+interface Skill {
+  name: string;
+  imgSrc: string;
+  invert?: boolean;
+}
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [TranslateModule,CommonModule,CdkDropList,  
-    CdkDragPreview,
-    CdkDropList,
-    CdkDragPreview,
-    CdkDrag,
+  imports: [TranslateModule,CommonModule
+
   ],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
@@ -18,54 +20,38 @@ import { CommonModule } from '@angular/common';
 
 })
 export class SkillsComponent {
-  // timelineItems = [
-  //   { aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg', position: 'right' },
-  //   { aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg', position: 'left' },
-  //   { aos: 'fade-up-right', imgSrc: '../../../../assets/imgs/express.svg', position: 'right' },
-  //   { aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg', position: 'left' },
-  //   { aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original-wordmark.svg', position: 'right' },
-  //   { aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/ionic/ionic-original.svg', position: 'left' },
-  //   { aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rxjs/rxjs-original.svg', position: 'right' },
-  //   { aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/socketio/socketio-original.svg', position: 'left', invert: true },
-  //   { aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg', position: 'right' },
-  //   { aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/swagger/swagger-original.svg', position: 'left' },
-  //   { aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg', position: 'right' },
-  //   { aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg', position: 'left' },
-  //   { aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg', position: 'right' },
-  //   { aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg', position: 'left' },
-  //   { aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuredevops/azuredevops-original.svg', position: 'right' },
-  //   { aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg', position: 'left', invert: true },
-  //   { aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gitlab/gitlab-original-wordmark.svg', position: '' },
-  //   { aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original-wordmark.svg', position: 'left' }
-  // ];
-  timelineItems = [
-    { type: 'component', aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg', position: 'right' },
-    { type: 'middle' },
-    { type: 'component', aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg', position: 'left' },
-    { type: 'component', aos: 'fade-up-right', imgSrc: '../../../../assets/imgs/express.svg', position: 'right' },
-    { type: 'middle' },
-    { type: 'component', aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg', position: 'left' },
-    { type: 'component', aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original-wordmark.svg', position: 'right' },
-    { type: 'middle' },
-    { type: 'component', aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/ionic/ionic-original.svg', position: 'left' },
-    { type: 'component', aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rxjs/rxjs-original.svg', position: 'right' },
-    { type: 'middle' },
-    { type: 'component', aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/socketio/socketio-original.svg', position: 'left', invert: true },
-    { type: 'component', aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg', position: 'right' },
-    { type: 'middle' },
-    { type: 'component', aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/swagger/swagger-original.svg', position: 'left' },
-    { type: 'component', aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg', position: 'right' },
-    { type: 'middle' },
-    { type: 'component', aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg', position: 'left' },
-    { type: 'component', aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg', position: 'right' },
-    { type: 'middle' },
-    { type: 'component', aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg', position: 'left' },
-    { type: 'component', aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuredevops/azuredevops-original.svg', position: 'right' },
-    { type: 'middle' },
-    { type: 'component', aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg', position: 'left', invert: true },
-    { type: 'component', aos: 'fade-up-right', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gitlab/gitlab-original-wordmark.svg', position: '' },
-    { type: 'middle', bottom: true },
-    { type: 'component', aos: 'fade-up-left', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original-wordmark.svg', position: 'left' }
+  frontendSkills: Skill[] = [
+    { name: 'Angular', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg' },
+    { name: 'React', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg' },
+    { name: 'Vue.js', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg' },
+    { name: 'Next.js', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg', invert: true },
+    { name: 'TypeScript', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg' },
+    { name: 'JavaScript', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg' },
+    { name: 'HTML5', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg' },
+    { name: 'CSS3', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg' },
+    { name: 'Bootstrap', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original-wordmark.svg' },
+    { name: 'Ionic', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/ionic/ionic-original.svg' },
+    { name: 'RxJS', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rxjs/rxjs-original.svg' }
   ];
+
+  backendSkills: Skill[] = [
+    { name: 'Node.js', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg' },
+    { name: 'Express', imgSrc: '../../../../assets/imgs/express.svg' },
+    { name: 'NestJS', imgSrc: '../../../../assets/imgs/nestjs.svg'},
+    { name: 'MongoDB', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original-wordmark.svg' },
+    { name: 'Mongoose', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongoose/mongoose-original-wordmark.svg' },
+    { name: 'Socket.io', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/socketio/socketio-original.svg', invert: true },
+    { name: 'Elasticsearch', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/elasticsearch/elasticsearch-original.svg' }
+  ];
+
+  toolsSkills: Skill[] = [
+    { name: 'Git', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg' },
+    { name: 'GitHub', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg', invert: true },
+    { name: 'GitLab', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gitlab/gitlab-original-wordmark.svg' },
+    { name: 'Azure DevOps', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuredevops/azuredevops-original.svg' },
+    { name: 'Postman', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg' },
+    { name: 'Swagger', imgSrc: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/swagger/swagger-original.svg' }
+  ];
+
 
 }
